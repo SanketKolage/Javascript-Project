9944b20493fb2893df89c204479bbdee0cbdc945
Sanket Kolage <60744054+SanketKolage@users.noreply.github.com>
2024-01-11 13:47:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import "./App.css";
+
+// Importing Components
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  // to input
+  const [inputText, setInputText] = useState("");
+  // to store
+  const [todos, setTodos] = useState([]);
+  // to check the status (complete or uncomplete)
+  const [status, setStatus] = useState("All");
+  // to filter todos
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  // Saving in LocalStorage
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let localTodos = JSON.parse(localStorage.getItem("todos"));
+      setTodos(localTodos);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>To-Do List</h1>
+      </header>
+
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+
+      <TodoList
+        filteredTodos={filteredTodos}
+        todos={todos}
+        setTodos={setTodos}
+      />
+    </div>
+  );
+}
+
+export default App;
